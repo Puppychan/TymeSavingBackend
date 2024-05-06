0e32a9099b4bce88a37fe491416184434755e04b
vietanh00 <s3928859@rmit.edu.vn>
2024-05-06 17:47:39 +0700

    user update
    TODO:
    - currently all 4 fields must be updated
    - after update, redirection doesnt work

--- src/functions/function_user.ts
+++ src/functions/function_user.ts
@@ -25,19 +25,110 @@ async function create_user(username: string, email:string, phone:string, passwor
 }
 
 // from username, fetch user details
-function read_user(){
-
+async function read_user(username: string): Promise<[number, any]> {
+    try{
+        const user = await User.findOne({'username': username});
+        if (user){
+            return [200, [user.username, user.user_phone, user.user_email, user.role, user.bankAccounts, user.user_points]];
+        }
+        else{
+            return [404, null];
+        }
+    } catch (err){
+        return [500, err];
+    }
 }
 
 // from provided information, update user details
-function update_user(){
+async function update_user(username: string, newUsername?: string, newEmail?: string, newPhone?: string, newPassword?: string): Promise<[number, string]>{
+    try {
+        const user = await User.findOne({ 'username': username });
 
+        if (!user) {
+            return [400, 'Cannot fetch this user'];
+        }
+
+        // Check if new username or email already exists
+        if (newUsername !== undefined && newUsername !== user.username && await exist_username(newUsername)) {
+            return [400, 'Invalid new username'];
+        }
+        if (newEmail !== undefined && newEmail !== user.user_email && await exist_email(newEmail)) {
+            return [400, 'Invalid new email'];
+        }
+
+        // Update user fields if new values are provided
+        if (newUsername !== undefined) {
+            user.username = newUsername;
+        }
+        if (newEmail !== undefined) {
+            user.user_email = newEmail;
+        }
+        if (newPhone !== undefined) {
+            user.user_phone = newPhone;
+        }
+        if (newPassword !== undefined) {
+            user.user_password = newPassword;
+        }
+        // Save the updated user object to the database
+        await user.save();
+
+        return [200, 'User updated successfully.'];
+    } catch (error) {
+        console.error('Error updating user:', error);
+        return [500, 'Internal Server Error'];
+    }
+    
 }
 
 // remove user with provided username
-function delete_user(){
-
+async function delete_user(username: string): Promise<[number, string]>{
+    try{
+        const query_user = await User.findOne({'username': username});
+        if(query_user){
+            User.deleteOne({'username': username});
+            return [200, 'User deleted successfully.'];
+        }
+        else{
+            return [400, 'No such user.'];
+        }
+    } catch (error){
+        console.log(error);
+        return [500, 'User could not be deleted.'];
+    }
 }
+
+// Check if username exists: return false if no such user exists, true otherwise
+async function exist_username(username:string): Promise<boolean>{
+    try{
+        const query_user = await User.findOne({'username': username});
+        if(query_user){
+            return true;
+        }
+        else{
+            return false;
+        }
+    } catch (error){
+        console.log(error);
+        return true;
+    }
+}
+
+// Check if email exists - we may use phone number in the future
+async function exist_email(email:string): Promise<boolean>{
+    try{
+        const query_user = await User.findOne({'user_email': email});
+        if(query_user){
+            return true;
+        }
+        else{
+            return false;
+        }
+    } catch (error){
+        console.log(error);
+        return true;
+    }
+}
+
 
 // TODO: Authentication and Authorization
 
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ dotenv.config();
 const app = express();
 app.use(json());
 
+// Serve static files from the 'public' directory
+app.use(express.static(__dirname+ '/views'));
+
 const port = process.env.PORT;
 
 // Parse URL-encoded bodies (as sent by HTML forms)
@@ -19,21 +22,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 connectMongoDB();
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server ' + process.env.MONGODB_URI);
-  res.send('hello');
+  res.send('Express + TypeScript Server');
 });
 
-// Handling sign up
+// Handling sign up i.e. creating user account
 app.get('/signup', (req, res) => {
-  res.sendFile(__dirname +'/views/signup.html');
+  res.sendFile(__dirname +'/views/user_create.html');
 });
 
-// Route handler for user signup
 app.post('/signup', async (req, res) => {
   console.log(req.body);
   const { username, email, phone, password } = req.body;
   try {
     const [response_code, response_msg] = await function_user.create_user(username, email, phone, password);
+    if(response_code == 200){
+      res.redirect('/user/' + username);
+    }
     res.status(response_code).send(response_msg);
   } catch (error) {
     console.error("Error creating user:", error);
@@ -41,6 +45,57 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+// Handling reading account information
+app.get('/user/:username', async (req, res) => {
+  const username = req.params.username;
+  try{
+    const [response_code, results] = await function_user.read_user(username);
+    res.status(response_code).send(results);
+  } catch (error){
+    res.status(500).send(error);
+  }
+
+});
+
+// Handling updating account information
+app.get('/user/:username/update', async (req, res) => {
+  // Show form to update user info + delete user account
+  res.sendFile(__dirname + '/views/user_update.html');
+});
+
+app.post('/user/:username/update', async (req, res) => {
+  const { username } = req.params;
+  const { newUsername, newEmail, newPhone, newPassword } = req.body;
+  console.log(req.body);
+  try {
+      // Call update_user function with provided parameters
+      const [response_code, response_msg] = await function_user.update_user(username, newUsername, newEmail, newPhone, newPassword);
+      console.log([response_code, response_msg]);
+      // this currently doesnt work
+      if(response_code == 200){
+        res.redirect('/user/' + username);
+      }
+      else{
+        res.status(response_code).send(response_msg);
+      }
+  } catch (error) {
+      console.error("Error updating user:", error);
+      res.status(500).send("Internal Server Error");
+  }
+});
+
+// Handling deleting account
+app.post('/user/:username/delete', async(req, res) => {
+  const { username } = req.params;
+  try {
+      // Call delete_user function with provided username
+      const [response_code, response_msg] = await function_user.delete_user(username);
+      res.status(response_code).send(response_msg);
+  } catch (error) {
+      console.error("Error deleting user account:", error);
+      res.status(500).send("Internal Server Error");
+  }
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
