f2d804de80686293eb576b42d4f783741c4ab523
Nhung <nhungmaitran1412@gmail.com>
2024-05-08 17:14:54 +0700

    fix: fix deploying process

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "../public/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          This is the backend server of TymeSaving
+        </p>
+      </div>
+    </main>
+  );
+}
