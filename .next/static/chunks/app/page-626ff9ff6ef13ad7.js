fa7d63d8449547b943e23706dfb7160f238c44e6
Nhung <nhungmaitran1412@gmail.com>
2024-05-08 16:53:52 +0700

    fix:fix deploying

---
+++ .next/static/chunks/app/page-626ff9ff6ef13ad7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{9226:function(n,e,i){Promise.resolve().then(i.t.bind(i,5320,23))},5320:function(n){n.exports={main:"page_main__NH83o",description:"page_description__lmVbt"}}},function(n){n.O(0,[66,971,23,744],function(){return n(n.s=9226)}),_N_E=n.O()}]);
