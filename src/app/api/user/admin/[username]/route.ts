d126dfc169bfe56237814f2bafaf27da112b2907
vietanh00 <s3928859@rmit.edu.vn>
2024-05-12 20:52:53 +0700

    correct output for admin viewing
    see admin/username/route.ts for example query
    
    TODO: turn query into search param

--- src/app/api/user/admin/[username]/route.ts
+++ src/app/api/user/admin/[username]/route.ts
@@ -1,35 +1,41 @@
-'use client'
+// 'use client'
 
 import { NextRequest, NextResponse } from "next/server";
-import { useSearchParams } from 'next/navigation';
+// import { useSearchParams } from 'next/navigation';
 import { connectMongoDB } from "../../../../../config/connectMongoDB";
 import User from "../../../../../models/user/model";
 
 /* The admin page will do the following:
   - Show the current user's information
   - List users in the database based on query parameters:
-      + byUsername (ascending/descending): sort the users by their username
+      + byUsername (ascending (1) /descending (-1)): sort the users by their username
       + byRole (true/false) separate users by role
-      + byCreation (ascending/descending): order the users by creation date
-  Example link: localhost:3000/api/user/admin/vietanh10?byUsername=ascending&byRole=true&byCreation=ascending
+      + byCreation (ascending (1) /descending (-1)): order the users by creation date
+  {
+    "byUsername": 1,
+    "byRole": "true",
+    "byCreation": -1
+}
 */
 
-export const GET = async (req: NextRequest, { params }: { params: { username: string } }) => {
-  const searchParams = useSearchParams();
+export const POST = async (req: NextRequest, { params }: { params: { username: string } }) => {
+  // const searchParams = useSearchParams();
   // Set default values 
-  // var byUsername: string|null = 'ascending'; //default: ascending
-  // var byRole: string|null = 'false';
-  // var byCreation: string|null = 'ascending';
-  // // Assign values from search parameter
-  // if(searchParams.has('byUsername') && ((searchParams.get('byUsername') === 'ascending' || searchParams.get('byUsername') === 'descending'))){
-  //   byUsername = searchParams.get('byUsername');
+  // var byUsername: string|null; //default: ascending
+  // var byRole: string|null;
+  // var byCreation: string|null;
+  // Assign values from search parameter
+  // const payload: JSON = JSON.parse(await req.json());
+  // if(payload.hasOwnProperty('byUsername') && ((payload['byUsername'] === 'ascending' || payload.get('byUsername') === 'descending'))){
+  //   byUsername = payload.get('byUsername');
   // }
-  // if(searchParams.has('byRole') && ((searchParams.get('byRole') === 'true' || searchParams.get('byUsername') === 'false'))){
-  //   byRole = searchParams.get('byRole');
+  // if(payload.hasOwnProperty('byRole') && ((payload.get('byRole') === 'true' || payload.get('byRole') === 'false'))){
+  //   byRole = payload.get('byRole');
   // }
-  // if(searchParams.has('byCreation') && ((searchParams.get('byCreation') === 'ascending' || searchParams.get('byCreation') === 'descending'))){
-  //   byCreation = searchParams.get('byCreation');
+  // if(payload.hasOwnProperty('byCreation') && ((payload.('byCreation') === 'ascending' || payload.get('byCreation') === 'descending'))){
+  //   byCreation = payload.get('byCreation');
   // }
+  var { byUsername= 1, byRole = 'false', byCreation  = 1 } = await req.json();
   try {
     await connectMongoDB();
     const user = await User.findOne({'username': params.username });
@@ -38,30 +44,28 @@ export const GET = async (req: NextRequest, { params }: { params: { username: st
     }
     // User found. Display their information and all users' information.
     // Query options based on query parameters
-    // var pipeline = [];
-    // // 1) Sort username
-    // if (byUsername === 'ascending' || byUsername === 'descending') {
-    //   var byUsernameParam = byUsername === 'ascending' ? 1 : -1
-    //   pipeline.push(
-    //     '{ $sort: { username: ' + byUsernameParam + '} }'
-    //   );
-    // }
-    // // 2) Group by role
-    // if (byRole === 'true') {
-    //   pipeline.push('{ $group: {  _id: "$role", users: { $push: "$$ROOT" }  } }');
-    // }
-    // // 3) Unwind
-    // pipeline.push('{  $unwind: "$users" }');
+    var aggregate = User.aggregate();
+    // 1) Sort username
+    if (byUsername === 1 || byUsername === -1) {
+      aggregate.append({ $sort: { username: byUsername } }) ;
+    }
+    // 2) Group by role
+    if (byRole === 'true') {
+      aggregate.append({ $group: {  _id: "$role", users: { $push: "$$ROOT" }  } });
+    }
+    // 3) Unwind
+    aggregate.unwind("$users");
     
-    // // 4) By creation date. Since MongoDB id embeds creation time, we can sort by id.
-    // if (byCreation === 'ascending' || byCreation === 'descending') {
-    //   var byCreationParam = byCreation === 'ascending' ? 1 : -1;
-    //   pipeline.push(' { $sort: { "users._id": ' + byCreationParam + ' } }');
-    // }
-    // // Output
-    // pipeline.push(' { $project: { username: "$users.username", role: "$users.role", email: "$users.email", fullname: "$users.fullname", phone: "$users.phone"  } }');
-    // let result = await User.aggregate({pipeline});
-    return NextResponse.json({ response: searchParams }, { status: 200 });
+    // 4) By creation date. Since MongoDB id embeds creation time, we can sort by id.
+    if (byCreation === 1 || byCreation === -1) {
+      // var byCreationParam:string = byCreation == 'ascending'? 'asc':'desc';
+      aggregate.append({ $sort: { "users._id": byCreation } });
+    }
+    // Output
+    aggregate.append({ $project: { username: "$users.username", role: "$users.role", email: "$users.email", fullname: "$users.fullname", phone: "$users.phone"  } });
+    let result = await aggregate.exec();
+
+    return NextResponse.json({ response: result }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ response: error.message}, { status: 500 });
   }
