0c842e5f7e39aa8abd4b4a62be53c33d304ad337
vietanh00 <s3928859@rmit.edu.vn>
2024-05-14 15:45:00 +0700

    Read, Update no longer return password

--- src/app/api/user/[username]/route.ts
+++ src/app/api/user/[username]/route.ts
@@ -6,11 +6,10 @@ import User from "src/models/user/model";
 export const GET = async (req: NextRequest, { params }: { params: { username: string } }) => {
   try {
     await connectMongoDB();
-    const user = await User.findOne({'username': params.username });
+    const user = await User.findOne({'username': params.username }).select('-password');
     if (!user) {
       return NextResponse.json({ response: 'User not found' }, { status: 404 });
     }
-
     return NextResponse.json({ response: user }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json({ response: error.message}, { status: 500 });
@@ -20,7 +19,7 @@ export const GET = async (req: NextRequest, { params }: { params: { username: st
 // Check if username exists: return false if no such user exists, true otherwise
 async function exist_username(username:string): Promise<boolean>{
   try{
-      const query_user = await User.findOne({'username': username});
+      const query_user = await User.findOne({'username': username}).select('-password');
       if(query_user){
           return true;
       }
@@ -36,7 +35,7 @@ async function exist_username(username:string): Promise<boolean>{
 // Check if email exists - we may use phone number in the future
 async function exist_email(email:string): Promise<boolean>{
   try{
-      const query_user = await User.findOne({'user_email': email});
+      const query_user = await User.findOne({'user_email': email}).select('-password');
       if(query_user){
           return true;
       }
@@ -89,11 +88,10 @@ try {
       }
       // Save the updated user object to the database
       await user.save();
-      // Return every user detail except the password
-      const userWithoutPassword = (({ password, ...rest }) => rest)(user);
-      console.log(userWithoutPassword);
+      // Fetch the data again, omit the password
+      const safeUser = await User.findOne(user._id).select('-password');
       // console.log(user);
-      return NextResponse.json({ response: userWithoutPassword }, { status: 200 });
+      return NextResponse.json({ response: safeUser }, { status: 200 });
   } catch (error) {
       console.error('Error updating user:', error);
       return NextResponse.json({ response: 'Cannot update user ' + username }, { status: 500 });
@@ -110,7 +108,7 @@ export const POST = async (req: NextRequest, { params }: { params: { username: s
       const username = params.username;
       try{
           await connectMongoDB();
-          const query_user = await User.findOne({'username': username});
+          const query_user = await User.findOne({'username': username}).select('-password');
           if(query_user){
               await User.deleteOne({'username': username});
               return NextResponse.json({ response: 'User deleted successfully.' }, { status: 200 });
