f3c025d1792fa571f0e88dc97b9637c65bad3125
vietanh00 <s3928859@rmit.edu.vn>
2024-06-05 19:30:17 +0700

    update tests directory
    thanks Khanh :>

--- src/tests/support-data.ts
+++ src/tests/support-data.ts
@@ -1,7 +1,11 @@
+import { UserRole } from "src/models/user/interface";
+
 export const defaultUser = {
   username: "hakhanhne",
   phone: "0938881145",
   email: "hakhanhne@gmail.com",
   password: "Rmit123@",
   fullname: "Khanh Tran",
+  role: UserRole.Admin,
+  pin: '1234'
 };
