6dcdff01c3ec4855a4f541f2e09ef40867820641
Nhung <nhungmaitran1412@gmail.com>
2024-06-11 12:05:29 +0700

    fix: fix deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
               { key: "Access-Control-Allow-Origin", value: "*" }, // later change if have specific domain from frontend
-              { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST" },
+              { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST, PUT, DELETE, PATCH" },
               { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Content-Length, Content-Type, Date, Authorization" },
             ]
           },
