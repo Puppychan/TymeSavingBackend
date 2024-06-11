8770b9e4d7da33672e19e7ca6d633bed599b0ebd
Nhung <nhungmaitran1412@gmail.com>
2024-06-11 12:09:51 +0700

    fix: fix deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,19 +7,10 @@ const nextConfig = {
             headers: [
               { key: "Access-Control-Allow-Credentials", value: "true" },
               { key: "Access-Control-Allow-Origin", value: "*" }, // later change if have specific domain from frontend
-              { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST, PUT, DELETE, PATCH" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Content-Length, Content-Type, Date, Authorization" },
+              { key: "Access-Control-Allow-Methods", value: "*" },
+              { key: "Access-Control-Allow-Headers", value: "*" },
             ]
           },
-        //   {
-        //     source: "/api/payment/vnpay",
-        //     headers: [
-        //       { key: "Access-Control-Allow-Credentials", value: "true" },
-        //       { key: "Access-Control-Allow-Origin", value: "https://production.vnpayment.vn" },
-        //       { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, POST" },
-        //       { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Content-Length, Content-Type, Date, Authorization" },
-        //     ]
-        //   }
         ];
       },
 };
