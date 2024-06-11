e611ab50d52a44822bd77072820e2e5b126709aa
hakhanhne <s3877707@rmit.edu.vn>
2024-06-11 13:30:10 +0700

    optimize upload file

--- src/app/api/upload-file/route.ts
+++ src/app/api/upload-file/route.ts
@@ -1,18 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getDownloadUrl, uploadFile } from 'src/lib/firebase/storage';
+import { uploadFile } from 'src/lib/firebase/storage';
 
 // request body as form data
 export const POST = async (req: NextRequest) => {
   try {
     const formData = await req.formData();
     const file = formData.get("file") as File;
-    let filename = file.name
-    let filename_clean = filename.split(' ').join('_')
-    const fileRef = `${Date.now()}_${filename_clean}`;
-    console.log(fileRef)
-    
-    await uploadFile(file, fileRef)
-    const downloadUrl = await getDownloadUrl(fileRef)
+
+    let filename = file.name.split(' ').join('_')
+    const fileRef = `${Date.now()}_${filename}`;
+
+    const downloadUrl = await uploadFile(file, fileRef)
 
     return  NextResponse.json({ response: downloadUrl }, { status: 200 });
   } catch (error: any) {
--- src/lib/firebase/storage.ts
+++ src/lib/firebase/storage.ts
@@ -1,24 +1,12 @@
 import {ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import { storage } from "src/config/firebase";
 
+
 export async function uploadFile(file: File | Blob | Uint8Array | ArrayBuffer, filename: string) {
   return new Promise(async (resolve, reject) => {
     try {
       const fileRef = ref(storage, filename)
-      const snapshot = await uploadBytes(fileRef, file)
-      console.log('Image Uploaded!');
-      resolve(snapshot)
-    }
-    catch(e)  {
-      reject(e)
-    }
-  })
-}
-
-export async function getDownloadUrl(imageRef: string) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const fileRef = ref(storage, imageRef)
+      await uploadBytes(fileRef, file)
       const downloadURL = await getDownloadURL(fileRef)
       console.log('File available at', downloadURL);
       resolve(downloadURL)
