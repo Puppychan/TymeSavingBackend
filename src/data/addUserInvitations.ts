99cfa0deb2c972f68d7f9836623e5e7ee3b4e81c
vietanh00 <s3928859@rmit.edu.vn>
2024-06-26 12:09:50 +0700

    invitation: byUser + admin

--- src/data/addUserInvitations.ts
+++ src/data/addUserInvitations.ts
@@ -8,21 +8,6 @@ export const addUserInvitations = async () => {
     const filePath = 'data/NI Data - UserInvitation.csv';
     try {
         var data = await readCSV(filePath);
-        data = data.map(userInvitation => {
-            if (userInvitation.status) {
-                if (userInvitation.status === 'Accepted'){
-                    userInvitation.status = UserInvitationStatus.Accepted;
-                }
-                else if (userInvitation.status === 'Cancelled'){
-                    userInvitation.status = UserInvitationStatus.Cancelled;
-                }
-                else {
-                    userInvitation.status = UserInvitationStatus.Pending;
-                }
-            }
-            console.log(userInvitation.status);
-            return userInvitation;
-        });
         await UserInvitation.insertMany(data);
         console.log('UserInvitations inserted successfully');
     } catch (error){
