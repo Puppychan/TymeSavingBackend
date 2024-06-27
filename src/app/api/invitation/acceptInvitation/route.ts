e69ea1364769faea27169b4c7c588665f12a9008
hakhanhne <s3877707@rmit.edu.vn>
2024-06-27 12:38:58 +0700

    done + tested integrate invitation + shared budget

--- src/app/api/invitation/acceptInvitation/route.ts
+++ src/app/api/invitation/acceptInvitation/route.ts
@@ -4,6 +4,8 @@ import Invitation from "src/models/invitation/model"
 import UserInvitation from "src/models/userInvitation/model";
 import {UserInvitationStatus} from "src/models/userInvitation/interface";
 import User from "src/models/user/model";
+import { InvitationType } from "src/models/invitation/interface";
+import { joinSharedBudget } from "src/lib/sharedBudgetUtils";
 /*
 Param: userId, invitationId  
 Pre-requisite: The user must have been invited i.e. must be in the invitation's 'users' array
@@ -49,6 +51,12 @@ export const POST = async (req: NextRequest) => {
         }
         // Save the updated invitation
         await invitation.save();
+
+        // ADD USER TO SHARED BUDGET GROUP
+        if (invitation.type === InvitationType.SharedBudget) {
+            await joinSharedBudget(userId, invitation.groupId)
+        }
+
         return NextResponse.json({ response: `User ${userId} accepted invitation ${invitationId}` }, { status: 200 });
     } catch (error){
         return NextResponse.json({ response: `Error: ${error}` }, { status: 500 });
