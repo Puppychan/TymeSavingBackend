c2dba36ee29e9e1695b6c23a93de8ccfa837ce5e
Nhung <nhungmaitran1412@gmail.com>
2024-07-04 12:59:33 +0700

    feat: add find by username to summary user

--- src/app/api/user/viewUserFromId/[userId]/route.ts
+++ src/app/api/user/viewUserFromId/[userId]/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from "next/server";
 import { connectMongoDB } from "src/config/connectMongoDB";
 import { verifyUser, newToken } from "src/lib/authentication";
